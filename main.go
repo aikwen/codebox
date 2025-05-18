ba14d57ee1f982d12aa32d455f63d1ee0a5d165a
kwen <kwen.zou@outlook.com>
2025-05-18 21:03:08 +0800

    router

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
-func main(){
-	fmt.Println("hello world")
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("hello codebox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`view page`))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w,"Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/codebox/view", snippetView)
+	mux.HandleFunc("/codebox/create", snippetCreate)
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
