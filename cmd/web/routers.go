4f11bee37bafe438e803352aaa24e60199740373
kwen <kwen.zou@outlook.com>
2025-06-16 16:35:21 +0800

    embed  files

--- cmd/web/routers.go
+++ cmd/web/routers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/aikwen/codebox/ui"
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
@@ -16,11 +17,10 @@ func (app *application) routes() http.Handler {
 		app.notFound(w)
 	})
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.FS(ui.Files))
 
 	router.Handler(http.MethodGet, "/static/*filepath",
-		http.StripPrefix("/static", fileServer))
-
+		fileServer)
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/codebox/view/:id", dynamic.ThenFunc(app.snippetView))
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,7 +4,9 @@ import (
 	"html/template"
 	"path/filepath"
 	"time"
+	"io/fs"
 
+	"github.com/aikwen/codebox/ui"
 	"github.com/aikwen/codebox/internal/models"
 )
 
@@ -32,33 +34,24 @@ var functions = template.FuncMap{
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files,"html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
 	for _, page := range pages {
 		// Extract the file name (like 'home.html') from the full filepath
 		name := filepath.Base(page)
+		patterns := []string{
+			"html/base.html",
+			"html/partials/*.html",
+			page,
+		}
 
-		// Parse the base template file into a template set.
-		// The template.FuncMap must be registered with the template set before you
-		// call the ParseFiles() method. This means we have to use template.New() to
-		// create an empty template set, use the Funcs() method to register the
-		// template.FuncMap, and then parse the file as normal.
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
-		// Call ParseGlob() *on this template set* to add any partials.
-		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
-		if err !=nil {
-			return nil, err
-		}
-		// Call ParseFiles() *on this template set* to add the page template.
-		ts, err = ts.ParseFiles(page)
-		if err !=nil {
-			return nil, err
-		}
+
 		cache[name] = ts
 	}
 
